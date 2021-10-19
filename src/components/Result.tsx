35c49d513b679efe1b67690db3b0d70ace2042f7
Kori Riddick <kori@koxa.io>
2021-10-19 13:21:56 -0400

    first commit add files

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,147 +1,187 @@
 import { useRef, useEffect } from "react";
 
 interface ResultProps {
-    children: React.ReactNode
-    aspectRatio: number
-    minimumGap: number
+  children: React.ReactNode;
+  aspectRatio: number;
+  minimumGap: number;
 }
 
 interface Dimensions {
-    width: number
-    height: number
+  width: number;
+  height: number;
 }
 
-export function Result({children, aspectRatio, minimumGap}: ResultProps) {
-    const container = useRef<HTMLDivElement | null>(null);
+export function Result({ children, aspectRatio, minimumGap }: ResultProps) {
+  const container = useRef<HTMLDivElement | null>(null);
 
-    useEffect(() => {
-        if (!container.current) return;
+  useEffect(() => {
+    if (!container.current) return;
 
-        const autoGrid = new AutoGridLogic();
-        const stopAutoGrid = autoGrid.connect(container.current, {
-            aspectRatio: aspectRatio,
-            minimumGap: minimumGap,
-        });
+    const autoGrid = new AutoGridLogic();
+    const stopAutoGrid = autoGrid.connect(container.current, {
+      aspectRatio: aspectRatio,
+      minimumGap: minimumGap,
+    });
 
-        return () => stopAutoGrid();
-    }, [container, aspectRatio, minimumGap]);
+    return () => stopAutoGrid();
+  }, [container, aspectRatio, minimumGap]);
 
-    return (
-        <div  ref={container} style={{
-            backgroundColor: '#fafafa',
-            position: 'absolute',
-            margin: '0 auto',
-            height: '80%',
-            width: '100%',
-            display: 'grid',
-            gridTemplateColumns: 'repeat(auto-fit, var(--item-width))',
-            gridTemplateRows: 'repeat(auto-fit, var(--item-height))',
-            placeContent: 'space-evenly'
-            
-        }}>
-            {children}
-        </div>
-    )
+  return (
+    <div
+      ref={container}
+      style={{
+        backgroundColor: "#fafafa",
+        position: "absolute",
+        margin: "0 auto",
+        height: "80%",
+        width: "100%",
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, var(--item-width))",
+        gridTemplateRows: "repeat(auto-fit, var(--item-height))",
+        placeContent: "space-evenly",
+      }}
+    >
+      {children}
+    </div>
+  );
 }
 
 interface ItemDimensionsConfig {
-    parentWidth: number;
-    parentHeight: number;
-    childrenCount: number;
-    requirements: AutoGridConfig;
+  parentWidth: number;
+  parentHeight: number;
+  childrenCount: number;
+  requirements: AutoGridConfig;
 }
 
 interface AutoGridConfig {
-    aspectRatio: number; // width / height
-    minimumGap: number;
+  aspectRatio: number; // width / height
+  minimumGap: number;
 }
 
 class AutoGridLogic {
-    /**
-     * connect will start monitoring a div element (which should have display: grid) for resize events.
-     * On resize events, the css grid items will automatically be resized based on the config's aspect ratio
-     * @param parentElement The HTMLDivElement that is a CSS Grid
-     */
-    public connect(parentElement: HTMLDivElement, autoGridConfig: AutoGridConfig) {
-        // listen for the container resizing
-        const resizeObserver = new ResizeObserver(() => {
-            if (parentElement && autoGridConfig) {
-                this.organizeContainerItems(parentElement, autoGridConfig);
-            }
-        });
+  /**
+   * connect will start monitoring a div element (which should have display: grid) for resize events.
+   * On resize events, the css grid items will automatically be resized based on the config's aspect ratio
+   * @param parentElement The HTMLDivElement that is a CSS Grid
+   */
+  public connect(
+    parentElement: HTMLDivElement,
+    autoGridConfig: AutoGridConfig
+  ) {
+    // listen for the container resizing
+    const resizeObserver = new ResizeObserver(() => {
+      if (parentElement && autoGridConfig) {
+        this.organizeContainerItems(parentElement, autoGridConfig);
+      }
+    });
 
-        // listen for children being added or removed
-        const childObserver = new MutationObserver(mutations => {
-            for (let i = 0; i < mutations.length; i++) {
-                if (mutations[i].type === 'childList') {
-                    this.organizeContainerItems(parentElement, autoGridConfig);
-                }
-            }
-        });
-
-        childObserver.observe(parentElement, { childList: true });
-        resizeObserver.observe(parentElement);
-
-        // Immediately resize items
-        requestAnimationFrame(() => {
-            const parentComputedStyles = window.getComputedStyle(parentElement);
-
-            if (parentComputedStyles.display !== 'grid') {
-                resizeObserver.unobserve(parentElement);
-                childObserver.disconnect();
-                return;
-            }
-
-            this.organizeContainerItems(parentElement, autoGridConfig);
-        });
-
-        return () => {
-            childObserver.disconnect();
-            resizeObserver.unobserve(parentElement);
-        };
-    }
-
-    private organizeContainerItems(parent: HTMLDivElement, requirements: AutoGridConfig) {
-        // If we do not have children, then we cannot resize them.
-        const children = parent.children;
-        if (!children.length) {
-            return;
+    // listen for children being added or removed
+    const childObserver = new MutationObserver((mutations) => {
+      for (let i = 0; i < mutations.length; i++) {
+        if (mutations[i].type === "childList") {
+          this.organizeContainerItems(parentElement, autoGridConfig);
         }
+      }
+    });
 
-        // Obtain the dimensions of the parent element
-        const parentDOMRect = parent.getBoundingClientRect();
-        // Get best item dimensions and apply property
-        const dimensions = this.getGridItemDimensions({
-            parentWidth: parentDOMRect.width,
-            parentHeight: parentDOMRect.height,
-            childrenCount: children.length,
-            requirements,
-        });
+    childObserver.observe(parentElement, { childList: true });
+    resizeObserver.observe(parentElement);
 
-        parent.style.setProperty('--item-width', dimensions.width + 'px');
-        parent.style.setProperty('--item-height', dimensions.height + 'px');
+    // Immediately resize items
+    requestAnimationFrame(() => {
+      const parentComputedStyles = window.getComputedStyle(parentElement);
+
+      if (parentComputedStyles.display !== "grid") {
+        resizeObserver.unobserve(parentElement);
+        childObserver.disconnect();
+        return;
+      }
+
+      this.organizeContainerItems(parentElement, autoGridConfig);
+    });
+
+    return () => {
+      childObserver.disconnect();
+      resizeObserver.unobserve(parentElement);
+    };
+  }
+
+  private organizeContainerItems(
+    parent: HTMLDivElement,
+    requirements: AutoGridConfig
+  ) {
+    // If we do not have children, then we cannot resize them.
+    const children = parent.children;
+    if (!children.length) {
+      return;
     }
 
-    /**
-     * Please perform all of your work in `getGridItemDimensions`. This is invoked when
-     * a div is added or removed, or when the window is resized. 
-     * 
-     * Use the information available to determine the width and height of each div.
-     * 
-     * @param config Information you can use to determine what the width & height of each item should be.
-     * @returns Dimensions - an object that describes the width and height of each item
-     */
-    private getGridItemDimensions(config: ItemDimensionsConfig): Dimensions {
+    // Obtain the dimensions of the parent element
+    const parentDOMRect = parent.getBoundingClientRect();
+    // Get best item dimensions and apply property
+    const dimensions = this.getGridItemDimensions({
+      parentWidth: parentDOMRect.width,
+      parentHeight: parentDOMRect.height,
+      childrenCount: children.length,
+      requirements,
+    });
 
-        console.log({config})
+    parent.style.setProperty("--item-width", dimensions.width + "px");
+    parent.style.setProperty("--item-height", dimensions.height + "px");
+  }
 
-        const height = 100
-        const width = height * config.requirements.aspectRatio
+  /**
+   * Please perform all of your work in `getGridItemDimensions`. This is invoked when
+   * a div is added or removed, or when the window is resized.
+   *
+   * Use the information available to determine the width and height of each div.
+   *
+   * @param config Information you can use to determine what the width & height of each item should be.
+   * @returns Dimensions - an object that describes the width and height of each item
+   */
+  private getGridItemDimensions(config: ItemDimensionsConfig): Dimensions {
+    console.log({ config });
 
-        return {
-            width,
-            height
-        }
-     
+    // let height = 100;
+    // let width = height * config.requirements.aspectRatio;
+
+    const areaOfContainer = config.parentHeight * config.parentWidth;
+    console.log({ areaOfContainer });
+    // let areaOfDiv = areaOfContainer / config.childrenCount;
+    // let heightDiv = Math.sqrt(areaOfDiv);
+    // let widthDiv = areaOfDiv / heightDiv;
+
+    let areaOfBoxes = 0;
+    let height = 100;
+    let width = 0;
+    let divHeight = 0;
+
+    // while (areaOfBoxes < areaOfContainer) {
+    //   height = height + 1;
+    //   width = height * config.requirements.aspectRatio;
+    //   areaOfBoxes = height * width * config.childrenCount;
+    //   console.log(areaOfBoxes);
+    // }
+
+    for (height = 100; areaOfBoxes < areaOfContainer; height++) {
+      divHeight = height / config.childrenCount;
+      width = height * config.requirements.aspectRatio;
+      areaOfBoxes = height * width * config.childrenCount;
     }
+
+    console.log(config.requirements.aspectRatio);
+    console.log(divHeight);
+    console.log(width);
+    console.log(areaOfBoxes);
+
+    //console.log(areaOfContainer);
+
+    // let numberOfColumns = config.parentHeight / lengthDiv;
+    // let numberOfRows = config.parentWidth / lengthDiv;
+
+    return {
+      width,
+      height: divHeight,
+    };
+  }
 }
